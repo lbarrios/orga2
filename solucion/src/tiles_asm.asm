5021d4084c9957f4208f67eaf753546f81c1b41c
Gonzalo Benegas <gbenegas@users.noreply.github.com>
2014-04-28 00:08:28 +0000

    puse mis archivos modificados

--- solucion/src/tiles_asm.asm
+++ solucion/src/tiles_asm.asm
@@ -1,6 +1,26 @@
 global tiles_asm
 
+extern printf
+
+
 section .data
+src: DQ 0
+dst: DQ 0
+filas: DD 0
+cols: DD 0
+src_row_size: DD 0
+dst_row_size: DD 0
+tamx: DD 0
+tamy: DD 0
+offsetx: DD 0
+offsety: DD 0
+ancho: DD 0
+ancho_tile: DD 0
+tile: DQ 0
+
+int_format: DB "%d", 10, 0
+
+
 
 section .text
 ;void tiles_asm(unsigned char *src,
@@ -10,5 +30,171 @@ section .text
 ;              int src_row_size,
 ;              int dst_row_size );
 
+
+
 tiles_asm:
+    ; armo stack frame
+    push rbp
+    mov rbp, rsp
+    ; tomo parametros
+    mov [src], rdi
+    mov [dst], rsi
+    mov [filas], edx
+    mov [cols], ecx
+    mov [src_row_size], r8d
+    mov [dst_row_size], r9d
+    mov eax, [rbp + 16]
+    mov [tamx], eax
+    mov eax, [rbp + 24]
+    mov [tamy], eax
+    mov eax, [rbp + 32]
+    mov [offsetx], eax
+    mov eax, [rbp + 40]
+    mov [offsety], eax
+    
+    ;armo mis variables
+%define f_d r8
+%define c_d r9
+%define f_t r10 
+%define c_t r11
+    ; r12 <- src_row_size
+    ; r13 <- dst
+%define dst_it r14
+    ; r15 <- tile
+%define ancho rdx
+%define ancho_tile rcx
+%define tile_it rsi
+
+    xor r8, r8
+    xor r9, r9
+    xor r10, r10
+    xor r11, r11
+    xor r12, r12
+    mov r12d, [src_row_size]
+    mov r13, [dst]
+    mov r14, r13
+    
+    mov r15, [src]
+    xor rax, rax
+    mov eax, [offsetx]
+    lea rax, [rax * 3]
+    add r15, rax
+    xor rax, rax
+    mov eax, [offsety]
+    mul r12d
+    ; resultado en edx:eax, espero que no se haya pasado
+    add r15, rax
+
+    xor rdx, rdx
+    mov edx, [cols]
+    lea rdx, [rdx * 3]
+    xor rcx, rcx
+    mov ecx, [tamx]
+    lea rcx, [rcx * 3]
+    mov rsi, r15
+
+
+
+
+.toma16: 
+    lea rdi, [c_d + 16]
+    cmp rdi, ancho
+    jg .conRetroceso
+    lea rdi, [c_t + 16]
+    cmp rdi, ancho_tile
+    jg .conRetrocesoTile
+    movdqu xmm1, [tile_it]
+    movdqu [dst_it], xmm1
+    add c_d, 16
+    add c_t, 16
+    add tile_it, 16
+    add dst_it, 16
+    jmp .toma16
+
+.conRetroceso: ;chequea ADEMAS que no te hayas pasado de tile
+    
+    ;jmp .fin
+    ;push rsi
+    ;push rdi
+    ;mov rdi, int_format
+    ;mov rsi, c_d
+    ;mov rax, 1
+    ;call printf
+    ;pop rdi
+    ;pop rsi
+
+
+    cmp c_d, ancho
+    je .nuevaFila
+    ; sea rdi lo que tengo que retroceder
+    mov rdi, c_d
+    add rdi, 16
+    sub rdi, ancho
+    sub c_d, rdi
+    sub c_t, rdi
+    sub tile_it, rdi
+    sub dst_it, rdi
+    jmp .toma16
+    
+.conRetrocesoTile: ;chequea ADEMAS que no te pases de ancho
+    cmp c_t, ancho_tile
+    je .nuevaFilaTile
+    ;sea rdi lo que tengo que retroceder
+    mov rdi, c_t
+    add rdi, 16
+    sub rdi, ancho_tile
+    sub c_d, rdi
+    sub c_t, rdi
+    sub tile_it, rdi
+    sub dst_it, rdi
+    jmp .toma16
+
+.nuevaFilaTile:
+    xor c_t, c_t
+    sub tile_it, ancho_tile
+    jmp .toma16
+
+.nuevaFila:
+    ;jmp .fin
+    inc f_d
+    inc f_t
+    ;cmp f_d, [cols] jajaj de nuevo lo de los 32
+    xor rax, rax
+    mov eax, [cols]
+    cmp f_d, rax
+    je .fin
+    ;cmp f_t, [tamy]
+    xor rax, rax
+    mov eax, [tamy]
+    cmp f_t, rax
+    je .vamosArribaTile
+    ; actualizo dst_it
+    sub dst_it, ancho
+    ; add dst_it, [dst_row_size] aca estaba accediendo a 64 envezd 32
+    xor rax, rax
+    mov eax, [dst_row_size]
+    add dst_it, rax
+    ; actualizo tile_it
+    sub tile_it, c_t
+    ;add tile_it, [src_row_size]
+    xor rax, rax
+    mov eax, [src_row_size]
+    add tile_it, rax
+    xor c_d, c_d
+    xor c_t, c_t
+    jmp .toma16
+
+.vamosArribaTile: ;ahora lo hacemos todolineal
+    xor f_t, f_t
+    sub dst_it, ancho
+    xor rax, rax
+    mov eax, [dst_row_size]
+    add dst_it, rax
+    mov tile_it, r15
+    xor c_d, c_d
+    xor c_t, c_t
+    jmp .toma16
+
+.fin:
+    pop rbp
     ret
--- solucion/src/tiles_c.c
+++ solucion/src/tiles_c.c
@@ -1,4 +1,3 @@
-
 #include "tp2.h"
 
 
@@ -21,8 +20,36 @@ void tiles_c    (
 		for (int j_d = 0, j_s = 0; j_d < cols; j_d++, j_s++) {
 			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
 			rgb_t *p_s = (rgb_t*)&src_matrix[i_s][j_s*3];
-			*p_d = *p_s;
+                        //*p_d = *p_s;
+                        //porque int?
+                        int ind_x = (i_d % tamy) + offsety;
+                        int ind_y = (j_d % tamx) + offsetx;
+                        p_d->b = src_matrix[ind_x][ind_y*3];
+                        p_d->g = src_matrix[ind_x][ind_y*3 + 1];
+                        p_d->r = src_matrix[ind_x][ind_y*3 + 2];
 		}
 	}
 
 }
+/*
+{
+    #define ancho  (col * 3)
+    #define ancho_tile (tamx * 3)
+    #define tile (src + offsetx * 3 + offsety * src_row_size)
+.ciclo_horizontal:
+    cmp ind_d_x + 16, ancho
+    jg .conRetroceso
+    cmp ind_t_x + 16, ancho_tile
+    jg .conRetroceso_tile
+    mov xmm, [tile[ind_t_x][ind_t_y]]
+    mov dest[ind_d_x][ind_d_y], xmm
+    jmp .ciclo_horizontal
+}
+*/
+
+
+
+
+
+
+
