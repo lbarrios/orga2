f33667a5ef106b434db8a8b58533dead428905c9
lbarrios <lbarrios@69fe60bf-0688-44b3-ac5c-7c9ab6183e10>
2014-05-06 22:24:49 +0000

    ...

--- solucion/src/temperature_c.c
+++ solucion/src/temperature_c.c
@@ -22,9 +22,42 @@ void temperature_c    (
 
 	for (int i_d = 0, i_s = 0; i_d < filas; i_d++, i_s++) {
 		for (int j_d = 0, j_s = 0; j_d < cols; j_d++, j_s++) {
-			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
 			rgb_t *p_s = (rgb_t*)&src_matrix[i_s][j_s*3];
-			*p_d = *p_s;
+			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
+			rgb_t color;
+			int t = ( p_s->r + p_s->g + p_s->b ) / 3;
+			if( t<32 ){
+				color.r = 0;
+				color.g = 0;
+				color.b = 128 + t*4;
+				*p_d = color;
+				continue;
+			}
+			if( t<96 ){
+				color.r = 0;
+				color.g = (t-32)*4;
+				color.b = 255;
+				*p_d = color;
+				continue;
+			}
+			if( t<160 ){
+				color.r = (t-96)*4;
+				color.g = 255;
+				color.b = 255 - ((t-96)*4);
+				*p_d = color;
+				continue;
+			}
+			if( t<224 ){
+				color.r = 255;
+				color.g = 255 - ((t-160)*4);
+				color.b = 0;
+				*p_d = color;
+				continue;
+			}
+			color.r = 255 - ((t-224)*4);
+			color.g = 0;
+			color.b = 0;
+			*p_d = color;
 		}
 	}
 }
