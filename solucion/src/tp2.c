e787725bff5186423f29ccfc91bc8190424623af
lbarrios <lbarrios@69fe60bf-0688-44b3-ac5c-7c9ab6183e10>
2014-05-08 14:53:10 +0000

    ...

--- solucion/src/tp2.c
+++ solucion/src/tp2.c
@@ -8,109 +8,140 @@
 #include "opencv_wrapper.h"
 #include "utils.h"
 
-DECLARAR_FILTRO(tiles)
-DECLARAR_FILTRO(popart)
-DECLARAR_FILTRO(ldr)
-DECLARAR_FILTRO(temperature)
+DECLARAR_FILTRO( tiles )
+DECLARAR_FILTRO( popart )
+DECLARAR_FILTRO( ldr )
+DECLARAR_FILTRO( temperature )
 
-filtro_t filtros[] = {
-	DEFINIR_FILTRO(tiles) ,
-	DEFINIR_FILTRO(popart) ,
-	DEFINIR_FILTRO(ldr) ,
-	DEFINIR_FILTRO(temperature) ,
-	{0,0,0,0}
+filtro_t filtros[] =
+{
+  DEFINIR_FILTRO( tiles ) ,
+  DEFINIR_FILTRO( popart ) ,
+  DEFINIR_FILTRO( ldr ) ,
+  DEFINIR_FILTRO( temperature ) ,
+  {0, 0, 0, 0}
 };
 
-void correr_filtro_video(configuracion_t *config, aplicador_fn_t aplicador);
-void correr_filtro_imagen(configuracion_t *config, aplicador_fn_t aplicador);
+void correr_filtro_video( configuracion_t* config, aplicador_fn_t aplicador );
+void correr_filtro_imagen( configuracion_t* config, aplicador_fn_t aplicador );
 
-int main( int argc, char** argv ) {
-
-	configuracion_t config;
-
-	procesar_opciones(argc, argv, &config);
-	// Imprimo info
-	if (!config.nombre)
-	{
-		printf ( "Procesando...\n");
-		printf ( "  Filtro             : %s\n", config.nombre_filtro);
-		printf ( "  Implementación     : %s\n", C_ASM( (&config) ) );
-		printf ( "  Archivo de entrada : %s\n", config.archivo_entrada);
-	}
-	
-	filtro_t *filtro = detectar_filtro(&config);
-
-	filtro->leer_params(&config, argc, argv);
-
-	if (config.es_video)
-		correr_filtro_video(&config, filtro->aplicador);
-	else
-		correr_filtro_imagen(&config, filtro->aplicador);
-
-	return 0;
-}
-
-filtro_t* detectar_filtro(configuracion_t *config)
+int main( int argc, char** argv )
 {
-	for (int i = 0; filtros[i].nombre != 0; i++)
-	{
-		if (strcmp(config->nombre_filtro, filtros[i].nombre) == 0)
-			return &filtros[i];
-	}
+  configuracion_t config;
+  procesar_opciones( argc, argv, &config );
 
-	perror("Filtro desconocido");
-	return NULL; // avoid C warning
+  // Imprimo info
+  if ( !config.nombre )
+  {
+    printf ( "Procesando...\n" );
+    printf ( "  Filtro             : %s\n", config.nombre_filtro );
+    printf ( "  Implementación     : %s\n", C_ASM( ( &config ) ) );
+    printf ( "  Archivo de entrada : %s\n", config.archivo_entrada );
+  }
+
+  filtro_t* filtro = detectar_filtro( &config );
+  filtro->leer_params( &config, argc, argv );
+
+  if ( config.es_video )
+  {
+    correr_filtro_video( &config, filtro->aplicador );
+  }
+  else
+  {
+    correr_filtro_imagen( &config, filtro->aplicador );
+  }
+
+  return 0;
 }
 
-void correr_filtro_imagen(configuracion_t *config, aplicador_fn_t aplicador)
+filtro_t* detectar_filtro( configuracion_t* config )
 {
-	snprintf(config->archivo_salida, sizeof(config->archivo_salida), "%s/%s.%s.%s%s.bmp",
-                 config->carpeta_salida, basename(config->archivo_entrada), 
-                 config->nombre_filtro, C_ASM(config), config->extra_archivo_salida );
-	
-	if (config->nombre)
-	{
-		printf("%s\n", basename(config->archivo_salida));
-	}
-	else
-	{
-		opencv_abrir_imagenes(config);
-		aplicador(config);
-		opencv_liberar_imagenes(config);
-	}
+  for ( int i = 0; filtros[i].nombre != 0; i++ )
+  {
+    if ( strcmp( config->nombre_filtro, filtros[i].nombre ) == 0 )
+    {
+      return &filtros[i];
+    }
+  }
+
+  perror( "Filtro desconocido" );
+  return NULL; // avoid C warning
 }
 
-void correr_filtro_video(configuracion_t *config, aplicador_fn_t aplicador)
+void correr_filtro_imagen( configuracion_t* config, aplicador_fn_t aplicador )
 {
-    if (!config->frames) {
-        snprintf(config->archivo_salida, sizeof(config->archivo_salida), "%s/%s.%s.%s%s.avi",
-                 config->carpeta_salida, basename(config->archivo_entrada),
-                 config->nombre_filtro, C_ASM(config), config->extra_archivo_salida );
-	}
-	
-	if (config->nombre)
-	{
-		printf("%s\n", basename(config->archivo_salida));
-	}
-	else
-	{
-		opencv_abrir_video(config);
-		opencv_frames_do(config, aplicador);
-		opencv_liberar_video(config);
-	}
+  snprintf( config->archivo_salida, sizeof( config->archivo_salida ), "%s/%s.%s.%s%s.bmp",
+            config->carpeta_salida, basename( config->archivo_entrada ),
+            config->nombre_filtro, C_ASM( config ), config->extra_archivo_salida );
+  snprintf( config->archivo_tiempo, sizeof( config->archivo_tiempo ), "%s/%s.%s.%s%s.txt",
+            config->carpeta_salida, basename( config->archivo_entrada ),
+            config->nombre_filtro, C_ASM( config ), config->extra_archivo_salida );
+
+  if ( config->nombre )
+  {
+    printf( "%s\n", basename( config->archivo_salida ) );
+  }
+  else
+  {
+    opencv_abrir_imagenes( config );
+
+    if ( config->tiempo )
+    {
+      FILE* t;
+      unsigned long start, end;
+      t = fopen( config->archivo_tiempo, "w" );
+
+      while ( config->cant_iteraciones-- )
+      {
+        MEDIR_TIEMPO_START( start );
+        aplicador( config );
+        MEDIR_TIEMPO_STOP( end );
+        fprintf( t, "%lu\n", ( end - start ) );
+      }
+
+      fclose( t );
+    }
+    else
+    {
+      aplicador( config );
+    }
+
+    opencv_liberar_imagenes( config );
+  }
+}
+
+void correr_filtro_video( configuracion_t* config, aplicador_fn_t aplicador )
+{
+  if ( !config->frames )
+  {
+    snprintf( config->archivo_salida, sizeof( config->archivo_salida ), "%s/%s.%s.%s%s.avi",
+              config->carpeta_salida, basename( config->archivo_entrada ),
+              config->nombre_filtro, C_ASM( config ), config->extra_archivo_salida );
+  }
+
+  if ( config->nombre )
+  {
+    printf( "%s\n", basename( config->archivo_salida ) );
+  }
+  else
+  {
+    opencv_abrir_video( config );
+    opencv_frames_do( config, aplicador );
+    opencv_liberar_video( config );
+  }
 }
 
 
 
-void imprimir_tiempos_ejecucion(unsigned long long int start, unsigned long long int end, int cant_iteraciones) {
-	unsigned long long int cant_ciclos = end-start;
-
-	printf("Tiempo de ejecución:\n");
-	printf("  Comienzo                          : %llu\n", start);
-	printf("  Fin                               : %llu\n", end);
-	printf("  # iteraciones                     : %d\n", cant_iteraciones);
-	printf("  # de ciclos insumidos totales     : %llu\n", cant_ciclos);
-	printf("  # de ciclos insumidos por llamada : %.3f\n", (float)cant_ciclos/(float)cant_iteraciones);
+void imprimir_tiempos_ejecucion( unsigned long long int start, unsigned long long int end, int cant_iteraciones )
+{
+  unsigned long long int cant_ciclos = end - start;
+  printf( "Tiempo de ejecución:\n" );
+  printf( "  Comienzo                          : %llu\n", start );
+  printf( "  Fin                               : %llu\n", end );
+  printf( "  # iteraciones                     : %d\n", cant_iteraciones );
+  printf( "  # de ciclos insumidos totales     : %llu\n", cant_ciclos );
+  printf( "  # de ciclos insumidos por llamada : %.3f\n", ( float )cant_ciclos / ( float )cant_iteraciones );
 }
 
 
