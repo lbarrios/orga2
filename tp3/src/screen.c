40c5fac1d925e065b0620b9e8bab2918e1444241
Gonzalo Benegas <gbenegas@users.noreply.github.com>
2014-06-16 05:04:16 +0000

    quedo mas lindo el esquema para pintar pantalla. queda mucho por delante

--- tp3/src/screen.c
+++ tp3/src/screen.c
@@ -4,23 +4,23 @@
 ================================================================================
   definicion de funciones del scheduler
 */
-
+/*
 #include "screen.h"
 #define SIZE_MAP 2500 // esto es el size en pixeles
 #define SIZE_PIXEL 2 // size en bytes
 #define MAP_FIRST_PIXEL 0xB8000 // esperemos que asi sea
 
-// los campos van a valer 0 todos si es fondo verde, y si no, el numero del tanque
+typedef enum {VERDE, INICIAL, PISADO, SUPERPUESTO, MINA, MISIL, MUERTO} Estado;
+typedef unsigned short pixel;
+typedef unsigned char Tank;
+
 typedef struct EstadoCasilla_s
 {            
-    char pisado;
-    char mina;
-    char muerto;
+    Estado current_state;
+    Tank tank_number;
 } EstadoCasilla;
 
-typedef unsigned short pixel;
-
-//EstadoCasilla map_state[SIZE_MAP];
+EstadoCasilla map_state[SIZE_MAP];
 
 void print_tank_context( int tank )
 {
@@ -28,6 +28,26 @@ void print_tank_context( int tank )
 }
 
 #define verdeCampo 0xA000
+#define ASCII_ZERO 48
+
+pixel colorea_pixel(EstadoCasilla s)
+{
+    Estado current_state = s.current_state;
+    Tank t = s.tank_number;
+    unsigned char color;
+    if (current_state == VERDE) color = COLOR_CAMPO;
+    else if (current_state == INICIAL) color = COLOR_INICIAL;
+    else if (current_state == PISADO) color = COLOR_PISADO;
+    else if (current_state == SUPERPUESTO) color = COLOR_SUPERPUESTO;
+    else if (current_state == MINA) color = COLOR_MINA;
+    else if (current_state == MISIL) color = COLOR_MISIL;
+    else if (current_state == MUERTO) color = COLOR_MUERTO;
+
+    pixel pixel_color = color<<8;
+    pixel pixel_ascii = t + ASCII_ZERO;
+    return color + ascii;
+}
+
 
 void print_map ()
 {
@@ -37,13 +57,12 @@ void print_map ()
         for (j = 0; j < 50; j++)
         {
             pixel *pixel_actual = ((pixel*)MAP_FIRST_PIXEL) + 80*i + j;
-            //EstadoCasilla estado_actual = map_state[i*50+j];
-            *pixel_actual = verdeCampo;//C_FG_BLUE + C_BG_BLUE;
-            //pixel_actual++;
+            EstadoCasilla estado_actual = map_state[i*50+j];
+            *pixel_actual = colorea_pixel(estado_actual);
         }
-        //pixel_actual += 30;
     }
 }
+*/
 /*
 void screen_proximo_reloj ()
 {
