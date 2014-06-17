5d63b48be8c299f3c29796f8184da4bb31b5635d
gsbenegas <gsbenegas@69fe60bf-0688-44b3-ac5c-7c9ab6183e10>
2014-06-17 20:40:11 +0000

    vamos adelante

--- tp3/src/game.c
+++ tp3/src/game.c
@@ -13,6 +13,35 @@ void game_inicializar() {
 }
 
 unsigned int game_mover(unsigned int id, direccion d) {
+
+
+    int nueva_pos;
+    EstadoCasilla estado_nueva_pos;
+    switch (estado_nueva_pos->current_state) {
+        case PASTO:
+            estado_nueva_pos->current_state = PISADO;
+            estado_nueva_pos->tank_number = NUMERODETANQUE;
+            mapear_pagina(args);
+            break;
+        case INICIAL:
+            if (estado_nueva_pos->)
+
+            color = COLOR_INICIAL; break;
+        case PISADO:
+            color = COLOR_PISADO; break;
+        case SUPERPUESTO:
+            color = COLOR_SUPERPUESTO; break;
+        case MINA:
+            color = COLOR_MINA; break;
+        case MISIL:
+            color = COLOR_MISIL; break;
+        case MUERTO:
+            color = COLOR_MUERTO; break;
+        default:
+            // Kernel bug?
+            break;
+    };
+ 
     return TRUE;
 }
 
