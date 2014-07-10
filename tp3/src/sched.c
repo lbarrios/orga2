e95d7c949bfe5a24e2f871be85168c2471ec4893
Ezequiel Barrios <lbarrios@users.noreply.github.com>
2014-07-10 00:20:13 +0000

    ..

--- tp3/src/sched.c
+++ tp3/src/sched.c
@@ -9,6 +9,7 @@
 #define TAREA_ACTUAL_IDLE 200
 
 unsigned char flag_pause = 0;
+unsigned char flag_idle = 0;
 unsigned char tarea_actual[2] = {TAREA_ACTUAL_IDLE, 0};
 unsigned char indice_actual = 7;
 unsigned char tss_actual = GDT_TASK1_DESCRIPTOR;
@@ -16,17 +17,20 @@ unsigned char tareas_muertas[8] = {0,0,0,0,0,0,0,0};
 
 unsigned long sched_proximo_indice()
 {
-  int i = 0;
-  do
+  if(flag_pause==1 || flag_idle==1)
   {
-    indice_actual = (indice_actual+1)%8;
-    i++;
-  } while (tareas_muertas[indice_actual] && i<8);
-
-  BD("indice actual = ")
-  BDPOINTER((long)indice_actual)
-  BDENTER()
-
+    indice_actual = TAREA_ACTUAL_IDLE;
+  }
+  else
+  {
+    int i = 0;
+    do
+    {
+      indice_actual = (indice_actual+1)%8;
+      i++;
+    } while (tareas_muertas[indice_actual] && i<8);
+    BD("indice actual = ")BDPOINTER((long)indice_actual)BDENTER()
+  }
   if(tss_actual == GDT_TASK1_DESCRIPTOR)
   {
     // Resguardo el contexto de la tarea de la tss_next_1
@@ -40,9 +44,20 @@ unsigned long sched_proximo_indice()
     {
       tss_tanques[tarea_a_resguardar] = tss_next_2;
     }
-    // Pongo la nueva tarea
-    tss_next_2 = tss_tanques[indice_actual];
-    tarea_actual[1] = indice_actual;
+    /* 
+      Pongo la nueva tarea
+    */
+    // Me fijo si va la idle o una tarea
+    if(flag_pause==1 || flag_idle==1)
+    {
+      tss_next_2 = tss_idle;
+      tarea_actual[1] = TAREA_ACTUAL_IDLE;
+    }
+    else
+    {
+      tss_next_2 = tss_tanques[indice_actual];
+      tarea_actual[1] = indice_actual;      
+    }
   }
   else
   {
@@ -57,9 +72,20 @@ unsigned long sched_proximo_indice()
     {
       tss_tanques[tarea_a_resguardar] = tss_next_1;
     }
-    // Pongo la nueva tarea
-    tss_next_1 = tss_tanques[indice_actual];
-    tarea_actual[0] = indice_actual;
+    /* 
+      Pongo la nueva tarea
+    */
+    // Me fijo si va la idle o una tarea
+    if(flag_pause==1 || flag_idle==1)
+    {
+      tss_next_1 = tss_idle;
+      tarea_actual[0] = TAREA_ACTUAL_IDLE;
+    }
+    else
+    {
+      tss_next_1 = tss_tanques[indice_actual];
+      tarea_actual[0] = indice_actual;
+    }
   }
 
   if(tss_actual == GDT_TASK1_DESCRIPTOR)
